127af6b0289ab846cf46b56210be98df86bcc070
Brian Whiting <brian@redpeppersoftware.com>
2022-12-07 15:30:22 -0700

    Added initial project structure

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client';
+import { App } from 'Components/App';
+
+const container = document.getElementById('root') as HTMLDivElement;
+const root = createRoot(container);
+
+root.render(<App />);
